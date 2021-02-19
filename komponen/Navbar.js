a109b99ea97ba6ca0e4876a4bcabb47a34554aa2
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-02-19 22:39:31 +0700

    Bagian-3

---
+++ komponen/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Blog EL Creative</h1>
+            </div>
+
+            <a>Home</a>
+            <a>Tentang</a>
+            <a>Postingan</a>
+        </nav>
+    );
+}
+ 
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Navbar from "../komponen/Navbar"
+import Footer from "../komponen/Footer"
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Selamat Datang di EL Creative Academy</h1>
+
+      <Footer />
+    </div>
+  )
+}
