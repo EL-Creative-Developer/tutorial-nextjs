d37e5acfe046a406e70e68233ec3cfadafcb1bc2
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-02-19 23:35:35 +0700

    Bagian-4

---
+++ komponen/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            Hak Cipta 2021 - <a href="https://www.elcreativeacademy.com/">EL Creative Academy</a>
+        </div>
+    );
+}
+ 
+export default Footer;
---
+++ pages/tentang.js
@@ -0,0 +1,10 @@
+const Tentang = () => {
+    return (
+        <div>
+            <h1>Tentang</h1>
+            <p>Ini adalah halaman tentang.</p>
+        </div>
+     );
+}
+ 
+export default Tentang;
